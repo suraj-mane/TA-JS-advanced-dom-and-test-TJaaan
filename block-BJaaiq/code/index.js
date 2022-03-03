361021a8e99d88f7820392ce2d05139fc02be278
suraj-mane <sun.mane1100@gmail.com>
2022-03-03 11:54:02 +0530

    update some code

--- block-BJaaiq/code/index.js
+++ block-BJaaiq/code/index.js
@@ -1,20 +1,35 @@
 
-quotes.forEach((ele) => console.log(ele.quoteText));
 let list = document.querySelector(".list");
+let max = 3;
+let index = 0;
 
-class Display {
-  constructor(arr){
-    this.arr = arr;
-  }
-  CreatUI(){
+function addQuote() {
+  for(let i = 0; i < max; i++){
     let li = document.createElement('li');
+    li.classList.add('lists');
     let p = document.createElement('p');
+    p.classList.add('name');
     let span = document.createElement('sapn');
-    p.innerText = this.quoteText;
-    span.innerText =this.quoteAuthor;
+    span.classList.add('author');
+    p.innerText = quotes[index].quoteText;
+    span.innerText = quotes[index].quoteAuthor;
     li.append(p, span);
     list.append(li);
+    index++;
   }
 }
 
-let d = new Display();
+
+document.addEventListener('scroll', () => {
+  let scrollTop = document.documentElement.scrollTop;
+  let scrollHeight = document.documentElement.scrollHeight;
+  let clientHeight = document.documentElement.clientHeight;
+  if(scrollTop + clientHeight >= scrollHeight && index < quotes.length){
+    addQuote();
+  }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  alert(`The Content of the DOM is Loaded`);
+  addQuote();
+});
